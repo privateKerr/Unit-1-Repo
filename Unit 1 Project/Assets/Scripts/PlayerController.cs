e1bf1511b2775a733fd5c31222f3b7d3f48f4a75
Hayden Kerr <10897906@my.uvu.edu>
2023-08-31 15:27:17 -0600

    Moved Project Location
    I had my Unity project in the wrong place on my computer, so I changed the file location

---
+++ Unit 1 Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public float gravity = -20f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
+
+        // Horizontal movement
+        float moveInput = Input.GetAxis("Horizontal");
+        Vector3 moveDirection = new Vector3(moveInput, 0, 0);
+        Vector3 move = transform.TransformDirection(moveDirection) * moveSpeed;
+
+        // Apply gravity
+        if (!isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0;
+        }
+
+        // Jumping
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+        }
+
+        // Apply movement and gravity
+        controller.Move((move + velocity) * Time.deltaTime);
+    }
+}
