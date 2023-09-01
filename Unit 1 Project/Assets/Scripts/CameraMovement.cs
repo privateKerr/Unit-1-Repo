5a7a0744b57c3d769808c193a91beddde2a28c08
Hayden Kerr <10897906@my.uvu.edu>
2023-09-01 16:54:41 -0600

    Camera, Audio, PowerUp Scripts
    Created a script for the camera to follow the player at a distance using AI. Added a jump sound effect and started working on a double jump power up. Still need to finish the PowerUp capabilities.

---
+++ Unit 1 Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //code from chatGPT
+    public Transform target; // The player's transform
+    public float smoothSpeed = 0.125f; // The speed at which the camera follows the player
+    public Vector3 offset; // The offset of the camera from the player
+
+    private void FixedUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
--- Unit 1 Project/Assets/Scripts/PlayerController.cs
+++ Unit 1 Project/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;  // The character controller component
     private Vector3 velocity;               // Current velocity of the character
 
+    public AudioSource jumpAudioSource;
+
     // Awake is called when the script instance is initialized
     private void Awake()
     {
@@ -38,6 +40,7 @@ public class PlayerController : MonoBehaviour
         // Jumping
         if (controller.isGrounded && Input.GetButtonDown("Jump"))
         {
+            jumpAudioSource.Play();
             velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
         }
 
